2fd6c984f1e54fe4c56258bbd1f9f3f25b070ddd
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-23 12:45:31 +0200

    updated paths in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import AppHeader from './common/AppHeader'
-import MarketPairs from './marketpairs'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/header';
+import MarketPairs from './containers/marketpairs'
 
 const App = () => (
-  <Router>
-    <React.Fragment>
-      <AppHeader />
-      <main role="main">
-        <div className="container-fluid">
-              <div className="row">
-                  <div className="col-12">
-                    <Route exact path="/" component={MarketPairs} />
-                  </div>
-              </div>
-          </div>
-      </main>
-    </React.Fragment>
-  </Router>
+    <Router basename="/Binance/">
+        <React.Fragment>
+            <Header/>
+            <main role="main">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-12">
+                            <Route exact path="/" component={MarketPairs}/>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </React.Fragment>
+    </Router>
 );
 
 export default App;
