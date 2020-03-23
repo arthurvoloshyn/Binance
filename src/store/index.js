97062bb15ea52a2481ae813683fc2acda1b4cfbf
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-23 12:41:10 +0200

    Updated store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
-import reducer from '../reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+import { save, load } from 'redux-localstorage-simple';
 
-export const store = createStore(
-    reducer,
-    applyMiddleware(logger)
-)
+import reducer from '../reducers';
+
+const composeEnhancers = process.env.NODE_ENV !== 'production' && typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const configureStore = preloadedState => createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(logger, save({ namespace: 'markets' }))));
+
+const preloadedState = load({ namespace: 'markets' }) || {};
+
+const store = configureStore(preloadedState);
 
 export default store
