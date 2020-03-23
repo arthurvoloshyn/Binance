093c971d4efe747508842c2e847b93db8486eb9d
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-23 14:52:12 +0200

    renamed header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/header';
+import Header from './components/Header';
 import MarketPairs from './containers/MarketPairs';
 
 const App = () => (
