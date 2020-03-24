984c820f7fc0b71068d224fbf5caf04d569f1a25
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-24 13:34:23 +0200

    Deleted bootstrap from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
