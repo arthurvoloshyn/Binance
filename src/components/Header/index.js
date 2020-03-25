01c8a11026a0a887915a9c0aec1489843a750fa8
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-25 18:32:21 +0200

    Edited header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,19 +9,7 @@ const Header = () => (
         <img src={logo} height="25" alt="Binance Logo" />
       </Link>
 
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="navbar-text">
         <span className="small text-white">Trade history viewer web app</span>
       </div>
     </nav>
