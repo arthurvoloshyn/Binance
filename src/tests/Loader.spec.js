8f5ca38aa3dba56c2cf9cf9778ef3ed825cafa91
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-25 17:53:51 +0200

    Added describe to test

--- src/tests/Loader.spec.js
+++ src/tests/Loader.spec.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from '../components/Loader';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+describe('Loader component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
 
-  ReactDOM.render(<Loader />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    ReactDOM.render(<Loader />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
