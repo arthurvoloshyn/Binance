6ca2323118a6958899a1bb68b9e3816bd1184ac9
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-26 17:51:11 +0200

    Edited test for marketPairs reducer

--- src/tests/reducers/marketPairs.spec.js
+++ src/tests/reducers/marketPairs.spec.js
@@ -2,21 +2,35 @@ import { UPDATE_MARKET_PAIRS } from '../../constants';
 import marketPairs, { initState } from '../../reducers/marketPairs';
 
 describe('marketPairs reducer', () => {
+  const action = {
+    type: UPDATE_MARKET_PAIRS,
+    data: {
+      BNBBTC: {},
+      ETHUSDT: {},
+      ENJBTC: {},
+    },
+  };
+
+  const { data } = action;
+
   it(`${UPDATE_MARKET_PAIRS}`, () => {
-    const action = {
-      type: UPDATE_MARKET_PAIRS,
-      data: {
-        BNBBTC: {},
-        ETHUSDT: {},
-        ENJBTC: {},
-      },
-    };
-
-    const { data } = action;
-
     expect(marketPairs(initState, action)).toEqual({
       ...initState,
       ...data,
     });
+  });
+
+  it(`should handle ${UPDATE_MARKET_PAIRS}`, () => {
+    expect(marketPairs(initState, action)).toMatchSnapshot();
+  });
+});
+
+describe('marketPairs reducer initial state', () => {
+  it('should return the initial state', () => {
+    expect(marketPairs(initState, {})).toEqual(initState);
+  });
+
+  it('returns properly', () => {
+    expect(marketPairs(initState, {})).toMatchSnapshot();
   });
 });
