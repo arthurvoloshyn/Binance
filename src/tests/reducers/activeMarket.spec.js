ac332e2ce9cce673f79d3cb591dd54aba5a26acf
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-26 14:58:18 +0200

    Edited test for activeMarket reducer

--- src/tests/reducers/activeMarket.spec.js
+++ src/tests/reducers/activeMarket.spec.js
@@ -2,16 +2,30 @@ import { SET_ACTIVE_MARKET } from '../../constants';
 import activeMarket, { initState } from '../../reducers/activeMarket';
 
 describe('activeMarket reducer', () => {
+  const action = {
+    type: SET_ACTIVE_MARKET,
+    data: {
+      market: 'BTC',
+    },
+  };
+
+  const { data } = action;
+
   it(`${SET_ACTIVE_MARKET}`, () => {
-    const action = {
-      type: SET_ACTIVE_MARKET,
-      data: {
-        market: 'BTC',
-      },
-    };
-
-    const { data } = action;
-
     expect(activeMarket(initState, action)).toEqual(data);
+  });
+
+  it(`should handle ${SET_ACTIVE_MARKET}`, () => {
+    expect(activeMarket(initState, action)).toMatchSnapshot();
+  });
+});
+
+describe('activeMarket reducer initial state', () => {
+  it('should return the initial state', () => {
+    expect(activeMarket(initState, {})).toEqual({});
+  });
+
+  it('returns properly', () => {
+    expect(activeMarket(initState, {})).toMatchSnapshot();
   });
 });
