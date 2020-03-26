890b1723e09c6bc3978943b0504ab0fcf2c24bb5
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-26 20:16:52 +0200

    Added test for Loader component

--- src/tests/components/Loader.spec.js
+++ src/tests/components/Loader.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Loader from '../components/Loader';
+import Loader from '../../components/Loader';
 
 describe('Loader component', () => {
   it('renders without crashing', () => {
