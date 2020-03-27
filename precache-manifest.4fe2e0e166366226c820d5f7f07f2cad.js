c0fafa80dcdab880b44e41317ad568c00909e948
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-27 14:18:03 +0200

    Updates

---
+++ precache-manifest.4fe2e0e166366226c820d5f7f07f2cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be08d40c70acb5dc9e33f4e95228b066",
+    "url": "/Binance/index.html"
+  },
+  {
+    "revision": "facebbf53bd820f6c679",
+    "url": "/Binance/static/css/2.6075bf79.chunk.css"
+  },
+  {
+    "revision": "db67c19536ecf3d9ef74",
+    "url": "/Binance/static/css/main.247d7d0b.chunk.css"
+  },
+  {
+    "revision": "facebbf53bd820f6c679",
+    "url": "/Binance/static/js/2.67b283be.chunk.js"
+  },
+  {
+    "revision": "db67c19536ecf3d9ef74",
+    "url": "/Binance/static/js/main.95e6f0d5.chunk.js"
+  },
+  {
+    "revision": "63ffe56965714144b197",
+    "url": "/Binance/static/js/runtime~main.6c4a5cdf.js"
+  },
+  {
+    "revision": "909daf684c253d6de14dfc7340545767",
+    "url": "/Binance/static/media/logo.909daf68.svg"
+  }
+]);
