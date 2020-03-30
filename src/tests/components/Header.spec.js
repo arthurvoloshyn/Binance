cc76b41ada1825af230c67ba89e25b8e474a52f4
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-03-30 18:00:45 +0300

    Added test for Header component

---
+++ src/tests/components/Header.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../../components/Header';
+
+describe('Header component', () => {
+  const header = shallow(<Header />);
+
+  it('renders properly', () => {
+    expect(header).toMatchSnapshot();
+  });
+
+  it('renders header', () => {
+    expect(header.find('header')).toHaveLength(1);
+  });
+
+  it('renders navbar text', () => {
+    expect(header.find('.navbar-text .small').text()).toEqual(
+      'Trade history viewer web app',
+    );
+  });
+});
